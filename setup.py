9461f78623621b523a79f12018552cbd7a9a69cc
Austin Scola <austinscola@gmail.com>
2021-07-10 06:31:48 -0400

    Add setup file

---
+++ setup.py
@@ -0,0 +1,38 @@
+"""A build script using setuptools for the pytest motor package."""
+import pathlib
+from typing import List
+
+import setuptools
+
+_HERE = pathlib.Path(__file__).parent
+
+_README = _HERE / 'README.md'
+_LONG_DESCRIPTION = _README.read_text()
+
+_VERSION_FILE = _HERE / 'VERSION.txt'
+_VERSION = _VERSION_FILE.read_text()
+
+_PACKAGES: List[str] = setuptools.find_packages(where=_HERE, include=['pytest_motor*'])
+
+setuptools.setup(
+    name='pytest_motor',
+    version=_VERSION,
+    author='Austin Scola',
+    author_email='austinscola@gmail.com',
+    description='An abstract syntax tree transformation tool for bettering Python code.',
+    long_description=_LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url='https://github.com/AustinScola/pytest_motor',
+    packages=_PACKAGES,
+    package_data={'pytest_motor': ['py.typed']},
+    entry_points={'pytest11': ['pytest_motor = pytest_motor.plugin']},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: Pytest',
+    ],
+    python_requires='>=3.6',
+    install_requires=['pytest', 'motor'],
+)
