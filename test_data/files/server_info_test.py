3a75482ceba37ecb209d8970fd3509776240ee5c
aleksul <aleksandrsulimov@bk.ru>
2021-07-16 21:47:04 +0300

    Update server info test

--- test_data/files/server_info_test.py
+++ test_data/files/server_info_test.py
@@ -1,9 +1,13 @@
 """A test file with a single test that retrieves the Motor client server info."""
 import pytest
+from pymongo.errors import ServerSelectionTimeoutError
 from motor.motor_asyncio import AsyncIOMotorClient
 
 
 @pytest.mark.asyncio
 async def test_using_motor_client_server_info(motor_client: AsyncIOMotorClient) -> None:
     """Test retrieving the Motor client server info."""
-    assert await motor_client.server_info() is not None
+    try:
+        await motor_client.server_info()
+    except ServerSelectionTimeoutError as exc:
+        assert False, f'MongoDB connection unsuccessful: {exc}'
