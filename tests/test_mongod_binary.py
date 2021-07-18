8ea534dd15251141df8f688c09ffcb58c57fbbc6
aleksul <aleksandrsulimov@bk.ru>
2021-07-18 19:26:19 +0300

    Rewrite tests with parametrize

--- tests/test_mongod_binary.py
+++ tests/test_mongod_binary.py
@@ -2,7 +2,7 @@
 import tempfile
 from pathlib import Path
 
-from pytest import MonkeyPatch, mark, raises
+from pytest import MonkeyPatch, mark, param
 
 from pytest_motor.mongod_binary import MongodBinary
 
@@ -15,128 +15,83 @@ MONGODB_WINDOWS_ZIP = Path(__file__).parent.parent / "test_data" / \
     "mongodb-windows-x86_64-4.4.6.zip"
 
 
+# yapf: disable
+@mark.parametrize(
+    'platform_name, distro_name, distro_version, result', [
+    param('Java', '', '', 'Java',
+        marks=mark.xfail(raises=OSError ,strict=True)),
+    param('Linux', 'ubuntu', '14.04', 'linux-x86_64-ubuntu1404',
+        marks=mark.xfail(raises=OSError ,strict=True)),
+    ('Linux', 'ubuntu', '16.04', 'linux-x86_64-ubuntu1604'),
+    ('Linux', 'ubuntu', '18.04', 'linux-x86_64-ubuntu1804'),
+    ('Linux', 'ubuntu', '20.04', 'linux-x86_64-ubuntu2004'),
+    ('Linux', 'ubuntu', '19.04', 'linux-x86_64-ubuntu1804'),
+    param('Linux', 'Debian', '8.0', 'linux-x86_64-debian80',
+        marks=mark.xfail(raises=OSError ,strict=True)),
+    ('Linux', 'debian', '9.2', 'linux-x86_64-debian92'),
+    ('Linux', 'debian', '10.0', 'linux-x86_64-debian10'),
+    ('Linux', 'debian', '9.0', 'linux-x86_64-debian92'),
+    param('Linux', 'opensuse', '8.0', 'linux-x86_64-opensuse80',
+        marks=mark.xfail(raises=OSError ,strict=True)),
+    ('Darwin', '', '', 'macos-x86_64'),
+    ('Windows', '', '', 'windows-x86_64'),
+    ]
+)
 @mark.filterwarnings("ignore::Warning")
-def test_mongo_platform(monkeypatch: MonkeyPatch) -> None:
+def test_mongo_platform(monkeypatch: MonkeyPatch,
+                        platform_name: str,
+                        distro_name: str,
+                        distro_version: str,
+                        result: str) -> None:
+    # yapf: enable
     """Test mongodb platform selection."""
 
-    path = Path(__file__).parent
-    # UNSUPPORTED OS
+    path = Path(tempfile.gettempdir())
 
-    monkeypatch.setattr("platform.system", lambda: "Java")
-    with raises(OSError) as excinfo:
-        # pylint: disable=expression-not-assigned
-        MongodBinary(path).current_os
-    assert "Your platform is unsupported." in str(excinfo.value)
-
-    # LINUX DISTROS
-
-    monkeypatch.setattr("platform.system", lambda: "Linux")
-
-    # UBUNTU
-
-    monkeypatch.setattr("distro.id", lambda: "ubuntu")
-
-    monkeypatch.setattr("distro.major_version", lambda: "14")
-    with raises(OSError) as excinfo:
-        # pylint: disable=expression-not-assigned
-        MongodBinary(path).current_platform
-    assert "Your Ubuntu version is too old. Upgrade at least to 16." in str(excinfo.value)
-
-    monkeypatch.setattr("distro.version", lambda: "16.04")
-    monkeypatch.setattr("distro.major_version", lambda: "16")
-    assert MongodBinary(path).current_platform == 'linux-x86_64-ubuntu1604'
-
-    monkeypatch.setattr("distro.version", lambda: "18.04")
-    monkeypatch.setattr("distro.major_version", lambda: "18")
-    assert MongodBinary(path).current_platform == 'linux-x86_64-ubuntu1804'
-
-    monkeypatch.setattr("distro.version", lambda: "20.04")
-    monkeypatch.setattr("distro.major_version", lambda: "20")
-    assert MongodBinary(path).current_platform == 'linux-x86_64-ubuntu2004'
-
-    monkeypatch.setattr("distro.version", lambda: "19.10")
-    monkeypatch.setattr("distro.major_version", lambda: "19")
-    assert MongodBinary(path).current_platform == 'linux-x86_64-ubuntu1804'
-
-    # DEBIAN
-
-    monkeypatch.setattr("distro.id", lambda: "debian")
-
-    monkeypatch.setattr("distro.major_version", lambda: "8")
-    with raises(OSError) as excinfo:
-        # pylint: disable=expression-not-assigned
-        MongodBinary(path).current_platform
-    assert "Your Debian version is too old. Upgrade at least to 9." in str(excinfo.value)
-
-    monkeypatch.setattr("distro.version", lambda: "9.2")
-    monkeypatch.setattr("distro.major_version", lambda: "9")
-    assert MongodBinary(path).current_platform == 'linux-x86_64-debian92'
-
-    monkeypatch.setattr("distro.version", lambda: "10.0")
-    monkeypatch.setattr("distro.major_version", lambda: "10")
-    assert MongodBinary(path).current_platform == 'linux-x86_64-debian10'
-
-    monkeypatch.setattr("distro.version", lambda: "9.0")
-    monkeypatch.setattr("distro.major_version", lambda: "9")
-    assert MongodBinary(path).current_platform == 'linux-x86_64-debian92'
-
-    # UNSUPPORTED DISTROS
-
-    monkeypatch.setattr("distro.id", lambda: "opensuse")
-    with raises(OSError) as excinfo:
-        # pylint: disable=expression-not-assigned
-        MongodBinary(path).current_platform
-    assert "Your distro (opensuse) is unsupported." in str(excinfo.value)
-
-    # MACOS
-
-    monkeypatch.setattr("platform.system", lambda: "Darwin")
-    assert MongodBinary(path).current_platform == 'macos-x86_64'
-
-    # WINDOWS
-
-    monkeypatch.setattr("platform.system", lambda: "Windows")
-    assert MongodBinary(path).current_platform == 'windows-x86_64'
+    monkeypatch.setattr("platform.system", lambda: platform_name)
+    monkeypatch.setattr("distro.id", lambda: distro_name)
+    monkeypatch.setattr("distro.version", lambda: distro_version)
+    monkeypatch.setattr("distro.major_version", lambda: distro_version.split('.')[0])
+    assert MongodBinary(path).current_platform == result
 
 
-def test_mongod_url(monkeypatch: MonkeyPatch) -> None:
+# yapf: disable
+@mark.parametrize(
+    'platform_name, distro_name, distro_version, true_url', [
+    ('Darwin', '', '',
+        "https://fastdl.mongodb.org/osx/mongodb-macos-x86_64-4.4.6.tgz"),
+    ('Linux', 'ubuntu', '18.04',
+        "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.4.6.tgz"),
+    ('Windows', '', '',
+        "https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-4.4.6.zip")
+    ]
+)
+def test_mongod_url(monkeypatch: MonkeyPatch,
+                    platform_name: str,
+                    distro_name: str,
+                    distro_version: str,
+                    true_url: str) -> None:
+    # yapf: enable
     """Test mongo version based on platform system."""
-    monkeypatch.setattr("platform.system", lambda: "Darwin")
-    assert MongodBinary(Path(__file__).parent).url == \
-        "https://fastdl.mongodb.org/osx/mongodb-macos-x86_64-4.4.6.tgz"
-
-    monkeypatch.setattr("platform.system", lambda: "Linux")
-    monkeypatch.setattr("distro.id", lambda: "ubuntu")
-    monkeypatch.setattr("distro.version", lambda: "18.04")
-    monkeypatch.setattr("distro.major_version", lambda: "18")
-    assert MongodBinary(Path(__file__).parent).url == \
-        "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.4.6.tgz"
-
-    monkeypatch.setattr("platform.system", lambda: "Windows")
-    assert MongodBinary(Path(__file__).parent).url == \
-        "https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-4.4.6.zip"
+    monkeypatch.setattr("platform.system", lambda: platform_name)
+    monkeypatch.setattr("distro.id", lambda: distro_name)
+    monkeypatch.setattr("distro.version", lambda: distro_version)
+    monkeypatch.setattr("distro.major_version", lambda: distro_version.split('.')[0])
+    assert MongodBinary(Path(tempfile.gettempdir())).url == true_url
 
 
-def test_unpack(monkeypatch: MonkeyPatch) -> None:
+@mark.parametrize('platform_name, archive_path', [('Darwin', MONGODB_MACOS_TAR),
+                                                  ('Windows', MONGODB_WINDOWS_ZIP)])
+def test_unpack(monkeypatch: MonkeyPatch, platform_name: str, archive_path: Path) -> None:
     """Test unpacking mechanism based on archive format."""
-    # pylint: disable=protected-access
+
     directory: Path = Path(tempfile.gettempdir())
 
-    monkeypatch.setattr("platform.system", lambda: "Darwin")
+    monkeypatch.setattr("platform.system", lambda: platform_name)
     binary_test = MongodBinary(directory)
-    assert binary_test.url.endswith('.tgz')
-    mongod_path = directory / "mongod"
-    with open(MONGODB_MACOS_TAR, 'rb') as test_file:
+    mongod_path = directory / binary_test.binary_name
+    with open(archive_path, 'rb') as test_file:
+        # pylint: disable=protected-access
         binary_test._MongodBinary__unpack(test_file)  # type: ignore[attr-defined]
     assert mongod_path.exists()
     mongod_path.unlink()
-
-    monkeypatch.setattr("platform.system", lambda: "Windows")
-    binary_test = MongodBinary(directory)
-    assert binary_test.url.endswith('.zip')
-    mongod_path = directory / "mongod.exe"
-    with open(MONGODB_WINDOWS_ZIP, 'rb') as test_file:
-        binary_test._MongodBinary__unpack(test_file)  # type: ignore[attr-defined]
-    assert mongod_path.exists()
-    mongod_path.unlink()
-    # pylint: enable=protected-access
