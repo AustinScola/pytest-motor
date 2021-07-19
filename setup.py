43a47e54e2537f618c636cefb910bf431b660ab3
aleksul <aleksandrsulimov@bk.ru>
2021-07-20 01:46:15 +0300

    Update from aiohttp to aiohttp[speedups]

--- setup.py
+++ setup.py
@@ -36,5 +36,5 @@ setuptools.setup(
         'Typing :: Typed',
     ],
     python_requires='>=3.6',
-    install_requires=['pytest', 'motor', 'aiohttp', 'distro'],
+    install_requires=['pytest', 'motor', 'aiohttp[speedups]', 'distro'],
 )
