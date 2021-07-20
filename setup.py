78c160a2b0d3e183423f07ad0cb59f9e251139ec
aleksul <aleksandrsulimov@bk.ru>
2021-07-20 11:21:16 +0300

    Add multiple python versions to setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,11 @@ setuptools.setup(
     entry_points={'pytest11': ['pytest_motor = pytest_motor.plugin']},
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Framework :: Pytest',
